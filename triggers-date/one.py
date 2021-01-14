fdca434a21ee569ceefd7d8725503e32bd781e4c
Michal Nowikowski <godfryd@gmail.com>
2021-01-14 15:39:42 +0100

    added triggers date example

---
+++ triggers-date/one.py
@@ -0,0 +1,21 @@
+def stage(ctx):
+    return {
+        "parent": "root",
+        "triggers": {
+            "date": "2021-01-14 18:00:00",
+        },
+        "parameters": [],
+        "configs": [],
+        "jobs": [{
+            "name": "hello world",
+            "steps": [{
+                "tool": "shell",
+                "cmd": "echo 'hello world'"
+            }],
+            "environments": [{
+                "system": "any",
+                "agents_group": "all",
+                "config": "default"
+            }]
+        }]
+    }
