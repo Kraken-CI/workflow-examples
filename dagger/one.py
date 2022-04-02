328e032d4d2a9cbdccf1350ffa89afe041836793
Michal Nowikowski <godfryd@gmail.com>
2022-04-02 15:17:20 +0200

    added example for dagger ci tool

---
+++ dagger/one.py
@@ -0,0 +1,34 @@
+def stage(ctx):
+    return {
+        "parent": "root",
+        "triggers": {
+            "parent": True,
+        },
+        "parameters": [],
+        "configs": [],
+        "jobs": [{
+            "name": "dagger",
+            "steps": [{
+                "tool": "shell",
+                "cmd": "cd /usr/local && curl -L https://dl.dagger.io/dagger/install.sh | sudo sh && dagger version"
+            }, {
+                "tool": "git",
+                "checkout": "https://github.com/dagger/dagger",
+                "branch": "v0.2.4"
+            }, {
+                "tool": "shell",
+                "cmd": "dagger do build --log-format plain",
+                "cwd": "dagger/pkg/universe.dagger.io/examples/todoapp"
+            }, {
+                "tool": "shell",
+                "cmd": "ls -al _build",
+                "cwd": "dagger/pkg/universe.dagger.io/examples/todoapp"
+            }],
+            "environments": [{
+                "system": "krakenci/bld-kraken",
+                "executor": "docker",
+            	"agents_group": "all",
+                "config": "default"
+            }]
+        }]
+    }
